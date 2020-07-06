2694863c17982faa8b525eae8bd160684d06cdb0
Felipe Souza <felipe.souza@dcomp.ufs.br>
2020-07-06 15:53:32 -0300

    Criar página de coleta de informações do contexto empresarial

---
+++ TWIndex/TWIndex/ViewModels/FormEmpresaViewModel.cs
@@ -0,0 +1,87 @@
+﻿using MVVMCoffee.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TWIndex.ViewModels
+{
+    public class FormEmpresaViewModel : BaseViewModel
+    {
+        public string _segmento;
+        public string Segmento
+        {
+            get { return _segmento; }
+            set
+            {
+                SetProperty(ref _segmento, value);
+                ValidarCommand.ChangeCanExecute();
+            }
+        } 
+        public string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                SetProperty(ref _nome, value);
+                ValidarCommand.ChangeCanExecute();
+            }
+        }
+
+        public string _cidade;
+        public string Cidade
+        {
+            get { return _cidade; }
+            set
+            {
+                SetProperty(ref _cidade, value);
+                ValidarCommand.ChangeCanExecute();
+            }
+        }
+
+        public string _estado;
+        public string Estado
+        {
+            get { return _estado; }
+            set
+            {
+                SetProperty(ref _estado, value);
+                ValidarCommand.ChangeCanExecute();
+            }
+        }
+
+        public Command ValidarCommand { get; private set; }
+
+        public string _valorStepper = "4";
+        public string ValorStepper
+        {
+            get { return _valorStepper; }
+
+            set { SetProperty(ref _valorStepper, value); }
+        }
+
+        public FormEmpresaViewModel()
+        {
+
+            ValidarCommand = new Command(
+          execute: () =>
+          {
+              int quantidadePalavras = int.Parse(ValorStepper);
+              Navigation.PushAsync<FormPalavrasViewModel>(false, quantidadePalavras);
+          },
+
+            canExecute: () =>
+            {
+                bool validador = false;
+
+                validador = !string.IsNullOrWhiteSpace(Segmento) &&
+                 !string.IsNullOrWhiteSpace(Nome);
+                 
+                return validador;
+            });
+
+        }
+
+    }
+}
---
+++ TWIndex/TWIndex/Views/FormEmpresaPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWIndex.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TWIndex.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FormEmpresaPage : ContentPage
+    {
+        public FormEmpresaPage()
+        {
+            InitializeComponent();
+            BindingContext = new FormEmpresaViewModel();
+        }
+
+        void OnStepperValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+
+            stpLabel.Text = string.Format("{0}", value);
+
+        }
+
+    }
+}
